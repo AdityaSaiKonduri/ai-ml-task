effb80d3dedd5128c9b0ed811459f737c882b0a1
AdityaSaiKonduri <adityakonduri8@gmail.com>
2024-10-20 11:52:44 +0530

    Source Code

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-key1 = "Thyroid and"
-# key2 = "Parathyroid"
-# new_key = "Thyroid and Parathyroid"
-
-# if key1 in result_dict and key2 in result_dict:
-#     result_dict[new_key] = result_dict[key2]
-#     # Remove the old keys
-#     del result_dict[key1]
-#     del result_dict[key2]
